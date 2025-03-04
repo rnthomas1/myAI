7ccfb17455e0e47153a96895cc5e2ac3949d942b
rnthomas1 <rnthomas0521@gmail.com>
2025-03-03 22:13:54 -0500

    Update ui.ts
    one more time

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Made By: Rachel T.`;
 export const CLEAR_BUTTON_TEXT: string = `CLEAR ALL`;
 export const PAGE_TITLE: string = `RTAI`;
-export const PAGE_DESCRIPTION: string = `Chat with Sam, Rachel's AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${"Sam"}, ${"Rachel"}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
