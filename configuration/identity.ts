fed8c7189e86f55ce8b336eddbf35ea0a4f2fdcd
rnthomas1 <rnthomas0521@gmail.com>
2025-03-03 22:38:43 -0500

    Update identity.ts
    update identity

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
+export const OWNER_NAME: string = `Rachel`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_NAME: string = `Sam`;
+export const AI_TONE: string = `Professional`;
+export const AI_ROLE: string = `AI Resume`;
