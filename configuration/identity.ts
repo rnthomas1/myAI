72a591b69bcb06def9d1e17d0e7f220025c9eec6
rnthomas1 <rnthomas0521@gmail.com>
2025-03-07 17:34:42 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Rachel`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `Rachel is a fourth year undergrad student at UNC Chapel Hill studying Business and Data Science`;
 
 export const AI_NAME: string = `Sam`;
 export const AI_TONE: string = `Professional`;
